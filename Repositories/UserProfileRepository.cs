c5e4b9b2a0c8c5686b2226004f3525b2a41047e2
Jordan Carter <jordan.carter.0023@gmail.com>
2021-04-09 15:24:01 -0500

    adding userProfiles

---
+++ Repositories/UserProfileRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Gifter.Models;
+using Gifter.Utils;
+
+
+namespace Gifter.Repositories
+{
+    public class UserProfileRepository : BaseRepository 
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<UserProfile> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT  up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated, 
+                       up.ImageUrl AS UserProfileImageUrl
+                        
+                       FROM UserProfile up
+                       ORDER BY [Name]";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var users = new List<UserProfile>();
+                    while (reader.Read())
+                    {
+                        users.Add(new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "UserProfileId"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                            ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                        });
+                    }
+                    reader.Close();
+
+                    return users;
+                }
+            }
+        }
+    }
+}
